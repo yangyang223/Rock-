722d00b894c0d76c64208281cdfdf99b7e4aca5a
yangyang223 <1412799935@qq.com>
2019-06-10 16:23:50 +0800

    创建项目

--- githubTest/main.cpp
+++ githubTest/main.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 #include<Windows.h>
-int main(void){
-std::cout<<"hello word!"<<std::endl;
-system("pause");
-return 0;
-}
+#include<string>
+using namespace std;
+	int main(void){
+		int i;
+		string ret;
+	for(i=0;i<3;i++){
+		cout<<"开始第"<<i+1<<"次相亲"<<endl;
+		cout<<"你喜欢打王者吗"<<endl;
+		cin>>ret;
+	if(ret!="yes"){
+		continue;
+	}
+	else{
+		cout<<"加个微信吧!"<<endl;
+	}   break;
+	}
+	cout<<"我中意你,你中意我吗?"<<endl;
+	cin>>ret;
+	if(ret=="中意"){
+		goto happy;
+	}
+	system("pause");
+	return 0;
+
+	happy:
+	 cout<<"开启甜蜜之旅..."<<endl;
+	 system("pause");
+	 return 0;
+	}
